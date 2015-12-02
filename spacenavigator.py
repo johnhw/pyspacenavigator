d9943930fed9ffed0d45d9e5fd0ed230632013d1
johnhw <johnhw@gmail.com>
2015-12-02 16:49:39 +0000

    First commit of spacenavigator.py

---
+++ spacenavigator.py
@@ -0,0 +1,137 @@
+from time import sleep
+import pywinusb.hid as hid
+import threading
+from collections import namedtuple
+import timeit
+
+# clock for timing
+high_acc_clock = timeit.default_timer
+
+## Simple HID code to read data from the 3dconnexion Space Navigator
+mappings={"y":[1, 1, 2], "x":[1, 3, 4], "z":[1,5,6], "roll":[2,1,2], "pitch":[2,3,4], "yaw":[2,5,6]}
+chan1_mappings = {k:v for k,v in mappings.iteritems() if v[0]==1}
+chan2_mappings = {k:v for k,v in mappings.iteritems() if v[0]==2}
+
+# convert two 8 bit bytes to a signed 16 bit integer
+def byte_2(y1,y2):
+    x = (y1) | (y2<<8)
+    if x>=32768:
+        x = -(65536-x)
+    return x
+    
+# state_3dx is a dictionary mapping [t,x,y,z,pitch,yaw,roll] to their latest values.
+# it is empty if the device has not been opened yet
+state_3dx = {}
+SpaceNavigator = namedtuple('SpaceNavigator', ['x', 'y', 'z', 'roll', 'pitch', 'yaw', 't'])
+state_tuple = None
+
+# make sure we don't have concurrent access to the current state
+state_lock = threading.Lock()
+
+# the ID for the space navigator
+cnx_hid_id = [0x046d,0xc626]
+
+_device = None
+
+def sample_handler(data, callback=None):
+    global state_tuple
+    """
+    Update the state based on the incoming data
+    The HID data is in the format
+    [id, a, b, c, d, e, f]
+    each pair (a,b), (c,d), (e,f) is a 16 bit signed value representing the absolute device state [from -350 to 350]
+    
+    if id==1, then the mapping is
+    (a,b) = y translation
+    (c,d) = x translation
+    (e,f) = z translation
+    
+    if id==2 then the mapping is
+    (a,b) = x tilting (roll)
+    (c,d) = y tilting (pitch)
+    (d,e) = z tilting (yaw)
+    
+    Each movement of the device always causes two HID events, one
+    with id 1 and one with id 2, to be generated, one after the other.
+    
+    This function updates the global state state_3dx, giving values for each
+    axis [x,y,z,roll,pitch,yaw] in range [-1.0, 1.0]
+    
+    The timestamp (in fractional seconds since the start of the program)  is written as element "t"
+    
+    If callback is provided, it is called on with a copy of the current state dictionary    
+    """
+    state_lock.acquire()
+    if data[0]==1:
+        for name,(chan,b1,b2) in chan1_mappings.iteritems():
+            state_3dx[name] = byte_2(data[b1], data[b2])/350.0
+    else:
+        for name,(chan,b1,b2) in chan2_mappings.iteritems():
+            state_3dx[name] = byte_2(data[b1], data[b2])/350.0
+    state_3dx["t"] = high_acc_clock()
+    if len(state_3dx)==7:
+        state_tuple = SpaceNavigator(**state_3dx)
+    state_lock.release()
+    
+    if callback:
+        callback(state_tuple)
+        
+   
+
+
+def close():
+    """Close the device, if it is open"""
+    if _device:
+        _device.close()
+        _device = None
+  
+def read():
+    """Return the current state of the navigation controller.
+    Returns:
+        state: {t,x,y,z,pitch,yaw,roll} dictionary
+        None if the device is not open.
+    """
+    return tuple_state
+    
+def open(callback=None):
+    """
+    Open the 3D space navigator device.
+    
+    Parameters:
+        callback: If callback is provided, it is called on each HID update with a copy of the current state dictionary    
+
+    Returns:
+        True if the device was opened successfully
+        False if the device could not be opened
+    """
+    # scan for HID devices, returns True if the device could be opened
+    all_hids = hid.find_all_hid_devices()
+    if all_hids:
+        for index, device in enumerate(all_hids):
+            if device.vendor_id == cnx_hid_id[0] and device.product_id == cnx_hid_id[1]:
+                print("3Dconnexion SpaceNavigator found")                
+                device.open()
+                if callback:
+                    device.set_raw_data_handler(lambda x:sample_handler(x,callback))
+                else:
+                    device.set_raw_data_handler(sample_handler)
+                _device = device
+                return True
+        print("No 3Dconnexion SpaceNavigator found")
+        return False
+    else:
+        print("No HID devices detected")
+        return False
+     
+
+def print_state(state):
+    # simple default printer callback
+    if state:
+        print " ".join(["%4s %+.2f"%(k,getattr(state,k)) for k in ['x', 'y', 'z', 'roll', 'pitch', 'yaw', 't']])
+
+if __name__ == '__main__':
+    open(callback=print_state)
+    while 1:
+        sleep(1)
+    
+        
