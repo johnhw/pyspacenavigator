c99bb47980ea17e103e9df5dcdb1a23451702a2b
johnhw <johnhw@gmail.com>
2015-12-02 18:24:49 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'spacenavigator',
+  packages = ['spacenavigator'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python interface to the 3DConnexion Space Navigator',
+  author = 'John Williamson',
+  author_email = 'johnhw@gmail.com',
+  url = 'https://github.com/johnhw/pyspacenavigator', # use the URL to the github repo
+  download_url = 'https://github.com/johnhw/pyspacenavigator/tarball/0.1',
+  keywords = ['spacenavigator', '3d', '6 DoF', 'HID'], 
+  classifiers = [],
+)
