86953f6277bf7e1461cefa160266524ecd4168ba
johnhw <johnhw@gmail.com>
2016-01-13 13:29:58 +0000

    Added better support for large numbers of buttons

--- README.md
+++ README.md
@@ -26,13 +26,39 @@ State objects returned from read() have 7 attributes: [t,x,y,z,roll,pitch,yaw,bu
 * button: bit 1: left button, bit 2: right button (e.g. 1=left, 2=right, 3=left+right, 0=none)
 
 ## API
-    open(callback=None, button_callback=None)      Open a connection to the device. Returns True on success
-                                                   If callback is given, it is called on each state change. 
-                                                   button_callback is called each time a button is pressed or released.
+    open(callback=None, button_callback=None)      
+        Open a 3D space navigator device. Makes this device the current active device, which enables the module-level read() and close()
+        calls. For multiple devices, use the read() and close() calls on the returned object instead, and don't use the module-level calls.
     
+        Parameters:        
+            callback: If callback is provided, it is called on each HID update with a copy of the current state namedtuple  
+            button_callback: If button_callback is provided, it is called on each button push, with the arguments (state_tuple, button_state) 
+            device: name of device to open. Must be one of the values in supported_devices. If None, chooses the first supported device found.            
+        Returns:
+            Device object if the device was opened successfully
+            None if the device could not be opened
+        
     read()              Return a namedtuple giving the current device state (t,x,y,z,roll,pitch,yaw,button)
-    close()             Close the connection to the device, if it is open
-    set_led(state)      Set the status of the LED to either on (True) or off (False)
+    close()             Close the connection to the current device, if it is open
+    set_led(state)      Set the status of the current devices LED to either on (True) or off (False)
+    list_devices()      Return a list of supported devices found, or an empty list if none found
+    
+    
+open() returns a DeviceSpec object. If you have multiple 3Dconnexion devices, you can use the object-oriented API to access them individually.
+Each object has the following API, which functions exactly as the above API, but on a per-device basis:
+
+    open()
+    read()
+    close()
+    set_led()
+    
+There are also attributes:
+    
+    connected       True if the device is connected, False otherwise
+    state           Convenience property which returns the same as read()
+    
+    
+    
     
 
 
--- spacenavigator.py
+++ spacenavigator.py
@@ -4,12 +4,13 @@ from collections import namedtuple
 import timeit
 import copy
 
+# current version number
+__version__=  "0.1.5"
+
 # clock for timing
 high_acc_clock = timeit.default_timer
 
 ## Simple HID code to read data from the 3dconnexion devices
-## TODO: does not support large numbers of buttons properly, because the bit mask keeps getting reset
-## (e.g. if buttons are sent on multiple data bytes)
 
 # convert two 8 bit bytes to a signed 16 bit integer
 def to_int16(y1,y2):
@@ -43,9 +44,19 @@ class DeviceSpec(object):
         """True if the device has been connected"""
         return self.device is not None
         
+    @property 
+    def state(self):
+        """Return the current value of read()
+        
+        Returns: state: {t,x,y,z,pitch,yaw,roll,button} namedtuple
+                None if the device is not open.
+        """
+        return self.read()
+        
     def open(self):
-        """Open a connection to the device"""
-        self.device.open()
+        """Open a connection to the device, if possible"""
+        if self.device:
+            self.device.open()
        
     def set_led(self, state):        
         """Set the LED state to state (True or False)"""
@@ -107,8 +118,7 @@ class DeviceSpec(object):
         If button_callback is provided, it is called only on button state changes with the argument (state, button_state).
         """
         
-        button_pushed = False
-        button_state = 0
+        button_pushed = False        
     
         for name,(chan,b1,b2,flip) in self.mappings.iteritems():
             if data[0] == chan:
@@ -116,13 +126,12 @@ class DeviceSpec(object):
                 
         for chan, byte, shift in self.button_mapping:
             if data[0] == chan:
-                button_pushed = True
-                button_state |= data[byte] << shift
-                
-        # update the button state
-        if button_pushed:
-            self.dict_state["button"] = button_state 
-            
+                button_pushed = True               
+                old_state = self.dict_state["button"]
+                # clear the relevant byte of old_state and set the new state in that space
+                old_state = old_state & ~((0xff)<<shift)
+                self.dict_state["button"] = old_state | (data[byte] << shift)
+                                        
         self.dict_state["t"] = high_acc_clock()
         
         # must receive both parts of the 6DOF state before we return the state dictionary
@@ -131,9 +140,7 @@ class DeviceSpec(object):
         
         # call any attached callbacks
         if self.callback:
-            self.callback(self.tuple_state)
-            
-            
+            self.callback(self.tuple_state)                        
         
         if self.button_callback and button_pushed:            
             self.button_callback(self.tuple_state, self.tuple_state.button)
@@ -146,12 +153,15 @@ device_specs = {
                         led_id=[0x8, 0x4b],
     
                         # axis mappings are specified as:
-                        # [channel, byte1, byte2, scale]; scale is usually just-1 or 1 and multiplies the result by this value 
+                        # [channel, byte1, byte2, scale]; scale is usually just -1 or 1 and multiplies the result by this value 
+                        # byte1 and byte2 are indices into the HID array indicating the two bytes to read to form the value for this axis
+                        # For the SpaceNavigator, these are consecutive bytes following the channel number. 
                         # (but per-axis scaling can also be achieved by setting this value)
                         mappings = {"x":[1, 1, 2,1], "y":[1, 3, 4,-1], "z":[1,5,6,-1], "pitch":[2,1,2,-1], "roll":[2,3,4,-1], "yaw":[2,5,6,1]},    
     
                         # button states are specified as:
                         # [channel, data byte, left bit shift to be applied]
+                        # If a message is received on the specified channel, the value of the data byte is applied
                         button_mapping = [(3,1,0)],
                         axis_scale = 350.0
                         ),        
@@ -166,7 +176,8 @@ def close():
         _active_device.close()
     
 def read():
-    """Return the current state of the active navigation controller.    
+    """Return the current state of the active navigation controller.
+    
     Returns:
         state: {t,x,y,z,pitch,yaw,roll,button} namedtuple
         None if the device is not open.
