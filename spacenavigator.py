87ae14ce291daf29ae83a5309218178e44bb319c
Kuba Andrýsek <33494544+JakubAndrysek@users.noreply.github.com>
2020-06-04 23:42:15 +0200

    Add SpaceMouse Pro
    Hi,
    I have tested new definition for SpaceMouse Pro.
    Jacob

--- spacenavigator.py
+++ spacenavigator.py
@@ -288,6 +288,38 @@ device_specs = {
         ],  # FIT
         axis_scale=350.0,
     ),
+    "SpaceMouse Pro": DeviceSpec(
+        name="SpaceMouse Pro",
+        # vendor ID and product ID
+        hid_id=[0x46D, 0xC62b],
+        led_id=[0x8, 0x4B],
+        mappings={
+            "x": AxisSpec(channel=1, byte1=1, byte2=2, scale=1),
+            "y": AxisSpec(channel=1, byte1=3, byte2=4, scale=-1),
+            "z": AxisSpec(channel=1, byte1=5, byte2=6, scale=-1),
+            "pitch": AxisSpec(channel=2, byte1=1, byte2=2, scale=-1),
+            "roll": AxisSpec(channel=2, byte1=3, byte2=4, scale=-1),
+            "yaw": AxisSpec(channel=2, byte1=5, byte2=6, scale=1),
+        },
+        button_mapping=[
+            ButtonSpec(channel=3, byte=1, bit=0),  # MENU
+            ButtonSpec(channel=3, byte=3, bit=7),  # ALT
+            ButtonSpec(channel=3, byte=4, bit=1),  # CTRL
+            ButtonSpec(channel=3, byte=4, bit=0),  # SHIFT
+            ButtonSpec(channel=3, byte=3, bit=6),  # ESC
+            ButtonSpec(channel=3, byte=2, bit=4),  # 1
+            ButtonSpec(channel=3, byte=2, bit=5),  # 2
+            ButtonSpec(channel=3, byte=2, bit=6),  # 3
+            ButtonSpec(channel=3, byte=2, bit=7),  # 4
+            ButtonSpec(channel=3, byte=2, bit=0),  # ROLL CLOCKWISE
+            ButtonSpec(channel=3, byte=1, bit=2),  # TOP
+            ButtonSpec(channel=3, byte=4, bit=2),  # ROTATION
+            ButtonSpec(channel=3, byte=1, bit=5),  # FRONT
+            ButtonSpec(channel=3, byte=1, bit=4),  # REAR
+            ButtonSpec(channel=3, byte=1, bit=1),  # FIT
+        ],  
+        axis_scale=350.0,
+    ),    
 }
 
 
